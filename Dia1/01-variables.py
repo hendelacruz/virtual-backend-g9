2da92a88c19c3c05e552a33d38fb0101d8515181
henry de la cruz orellana <hendelacruz@gmail.com>
2021-08-21 22:36:41 -0500

    feat:added a new file variables.py

---
+++ Dia1/01-variables.py
@@ -0,0 +1,26 @@
+# Esto es un comentario
+
+#variables de texto(es igual"" o '')
+nombre = "eduardo"
+apellido = 'de rivero'
+
+#comilla doble por que al interior hay una comilla simple
+direccion = "calle o'higgins 125" 
+
+#se utilizan triple "" o triple '' para texto de varias lineas con salto de linea, tabulaciones
+texto = """hola amigos:
+buenas noches, les saluda su profe
+            atentamente,
+                    eduardo"""
+
+texto2 = '''hola amigos:
+ya casi viene el break'''
+
+#variables numericas
+año = 2021
+estatura = 1.70
+
+#type es una funcion que me muestra que tipo de dato es lo que vaya dentro del ()
+#print es imprimir
+print(type(nombre))
+print(type(año))
