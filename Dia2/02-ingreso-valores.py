7f7b55f99a177988f3510c295762680d9297dde5
henry de la cruz orellana <hendelacruz@gmail.com>
2021-08-23 10:05:00 -0500

    feat:added a new file ingreso-valores

---
+++ Dia2/02-ingreso-valores.py
@@ -0,0 +1,6 @@
+resultado = input("Ingresa tu nombre:")
+print(resultado)
+print(type(resultado))
+#no importa si el input es int, float, etc, siempre sera un string
+# para cambiar mi variable resultado a entero
+#print(int(resultado))
