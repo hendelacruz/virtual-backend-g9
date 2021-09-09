7005a22dc7c0ae598287b2f3a3b53d788844cc50
henry de la cruz orellana <hendelacruz@gmail.com>
2021-09-09 13:49:07 -0500

    chore: avance proyecto reposteria

--- reposteria/app.py
+++ reposteria/app.py
@@ -4,21 +4,23 @@ from models.ingrediente import IngredienteModel
 from models.receta import RecetaModel
 from models.preparacion import PreparacionModel
 from models.recetas_ingredientes import RecetaIngredienteModel
-# from models.log import LogModel
+from models.log import LogModel
 
-# from controllers.ingrediente import (IngredientesController,
-#                                      IngredienteController,
-#                                      FiltroIngredientesController)
-# from controllers.receta import RecetasController
-# from flask_restful import Api
+from controllers.ingrediente import (IngredientesController,
+                                     IngredienteController,
+                                     FiltroIngredientesController)
+# from controllers.receta import RecetasController, RecetaController
+# from controllers.receta_ingrediente import RecetaIngredientesController
+# from controllers.preparacion import PreparacionesController
+
+from flask_restful import Api
 from os import environ
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-# api = Api(app=app)
+api = Api(app=app)
 #                                        mysql://username:password@host/db_name
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Hdo1983@localhost:3306/reposteria_flask'
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 # si se establece True SqlAchemy rastreara las modificaciones de los objetos (modelos) y lanzara señales de cambio, su valor predeterminado es None . igual habilita el tracking pero emite una advertencia que en futuras versiones se removera el valor x default None y si o si tendremos que indicar un valor inicial
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,7 +29,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 base_de_datos.init_app(app)
 
 # eliminara todas las tablas registradas en nuestro proyecto
-#base_de_datos.drop_all(app=app)
+# base_de_datos.drop_all(app=app)
 
 
 # creara las tablas aun no mapeadas y si todo esta bien no devolvera nada
@@ -41,11 +43,16 @@ def initial_controller():
     }
 
 
-# # ZONA DE ENRUTAMIENTO
-# api.add_resource(IngredientesController, '/ingredientes')
+# ZONA DE ENRUTAMIENTO
+api.add_resource(IngredientesController, '/ingredientes')
 # api.add_resource(IngredienteController, '/ingrediente/<int:id>')
 # api.add_resource(FiltroIngredientesController, '/buscar_ingrediente')
+
 # api.add_resource(RecetasController, '/recetas')
+# api.add_resource(RecetaController, '/receta/<int:id>')
+# api.add_resource(RecetaIngredientesController, '/recetas_ingredientes')
+# api.add_resource(PreparacionesController, '/preparaciones',
+#                  '/preparaciones/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
